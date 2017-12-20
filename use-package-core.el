891ea09ffd3573e8a1da738bffdeccb8e3af44f0
Justin Burkett <justin@burkett.cc>
2017-12-20 08:52:45 -0500

    Fix formatting of use-package-report table
    Use org-table-align to make it readable.

--- use-package-core.el
+++ use-package-core.el
@@ -972,6 +972,9 @@ meaning:
                      (float-time (gethash :preface-secs hash 0))
                      (float-time (gethash :use-package-secs hash 0))))))
      use-package-statistics)
+    (require 'org-table)
+    (org-table-align)
+    (goto-char (point-min))
     (display-buffer (current-buffer))))
 
 (defun use-package-statistics-gather (keyword name after)
