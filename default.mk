caa92f1d64fc25480551757d854b4b49981dfa6b
John Wiegley <johnw@newartisans.com>
2020-11-10 13:33:45 -0800

    Update version to 2.4.1

--- default.mk
+++ default.mk
@@ -63,9 +63,9 @@ ELGS =
 
 ## Versions ##########################################################
 
-VERSION = 2.4
+VERSION = 2.4.1
 
-USE_PACKAGE_VERSION = 2.4
+USE_PACKAGE_VERSION = 2.4.1
 
 EMACS_VERSION = 24.3
 
